9092294c30e05b71ea61b0afd16fd6daa8c7bb32
Ctibor Škuta <skuta.ctibor@gmail.com>
2011-11-29 12:25:46 +0100

    make_skos fnc added

--- handler.py
+++ handler.py
@@ -12,15 +12,17 @@ OPTIONS
    -a -iteratively updates each subject in database, it takes a long time but during the process the database is still prepared for use
 """
 
-import os, sys, time, urllib2, math, re, datetime
+import os, sys, time, urllib2, math, re
+from itertools import *
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_local'
 
-from psh.models import Hesla, Varianta, Ekvivalence, Hierarchie, Topconcepts, Pribuznost, Zkratka, SysNumber
+from psh.models import Hesla, Varianta, Ekvivalence, Hierarchie, Topconcepts, Pribuznost, Zkratka, SysNumber, Aktualizace
 
 from django.conf import settings
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.db import connection
 
 conceptTable = u"""
             <table>
@@ -365,8 +367,77 @@ def isValid(num):
     else:
         return False
 
+def query_to_dicts(query_string, *query_args):
+    """Run a simple query and produce a generator
+    that returns the results as a bunch of dictionaries
+    with keys for the column values selected.
+    """
+    cursor = connection.cursor()
+    cursor.execute(query_string, query_args)
+    col_names = [desc[0] for desc in cursor.description]
+    while True:
+        row = cursor.fetchone()
+        if row is None:
+            break
+        row_dict = dict(izip(col_names, row))
+        yield row_dict
+    return
+
+def get_concept_as_dict(subject_id):
+    """Get concept as dict from database according to its PSH ID"""
+    heslo = query_to_dicts("""SELECT hesla.id_heslo, 
+        hesla.heslo,
+        ekvivalence.ekvivalent
+        FROM hesla
+        LEFT JOIN ekvivalence ON ekvivalence.id_heslo = hesla.id_heslo
+        WHERE hesla.id_heslo = '%s'""" %subject_id)
+    
+    varianty = query_to_dicts("""SELECT varianta,
+        jazyk
+        FROM varianta
+        WHERE id_heslo = '%s'""" %subject_id)
+    
+    podrazeny = query_to_dicts("""SELECT podrazeny
+        FROM hierarchie
+        WHERE nadrazeny = '%s'""" %subject_id)
+    
+    nadrazeny = query_to_dicts("""SELECT nadrazeny
+        FROM hierarchie
+        WHERE podrazeny = '%s'""" %subject_id)
+
+    pribuzny = query_to_dicts("""SELECT pribuzny
+        FROM pribuznost
+        WHERE pribuznost.id_heslo = '%s'""" %subject_id)
+    
+    hesla = list(heslo)
+    if hesla:
+        heslo = hesla[0]
+
+        heslo["nadrazeny"] = ""
+        for n in nadrazeny:
+            heslo["nadrazeny"] = n["nadrazeny"]
+
+        heslo["podrazeny"] = []
+        heslo["pribuzny"] = []
+        heslo["varianty"] = []
+
+        for p in podrazeny:
+                heslo["podrazeny"].append(p["podrazeny"])
+        for p in pribuzny:
+                heslo["pribuzny"].append(p["pribuzny"])
+        for v in varianty:
+                heslo["varianty"].append({"varianta": v["varianta"], "jazyk": v["jazyk"]})
+    else:
+        heslo = None
+    return heslo
+
+def save_update_time():
+    update_time = Aktualizace()
+    update_time.save()
+
 def update():
     """Inititates update of PSH database."""
+    save_update_time()
     if len(sys.argv) > 1:
         all = sys.argv[1]
         if all == "-n":
@@ -378,7 +449,140 @@ def update():
     else:
         all = False
     updatePSH(all)
-    
+
+def make_skos():
+    header = """<?xml version="1.0" encoding="utf-8"?>
+<rdf:RDF
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:dcterms="http://purl.org/dc/terms/"
+   xmlns:dctype="http://purl.org/dc/dcmitype/"
+   xmlns:foaf="http://xmlns.com/foaf/0.1/"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:skos="http://www.w3.org/2004/02/skos/core#"
+   xmlns:xsd="http://www.w3.org/2001/XMLSchema#">
+  <skos:ConceptScheme rdf:about="http://psh.ntkcz.cz/skos/">
+    <cc:attributionName xml:lang="en">National Technical Library</cc:attributionName>
+    <cc:attributionName xml:lang="cs">Národní technická knihovna</cc:attributionName>
+    <cc:attributionURL rdf:resource="http://www.techlib.cz/cs/katalogy-a-databaze/psh/"/>
+    <cc:legalcode rdf:resource="http://creativecommons.org/licenses/by-nc-sa/3.0/cz/"/>
+    <cc:license rdf:resource="http://creativecommons.org/licenses/by-nc-sa/3.0/cz/"/>
+    <cc:morePermissions rdf:resource="http://www.techlib.cz/cs/katalogy-a-databaze/psh/"/>
+    <dc:creator>
+      <rdf:Description>
+        <foaf:mbox rdf:resource="mailto:psh@stk.cz"/>
+        <foaf:name xml:lang="en">National Technical Library</foaf:name>
+        <foaf:name xml:lang="cs">Národní technická knihovna</foaf:name>
+      </rdf:Description>
+    </dc:creator>
+    <dc:description xml:lang="cs">Polytematický strukturovaný heslář je česko-anglický řízený a měnitelný slovník lexikálních jednotek. Slouží k vyjádření věcného obsahu dokumentů a ke zpětnému vyhledání dokumentů na základě věcných kritérií a je určen především pro knihovny s polytematickými fondy.</dc:description>
+    <dc:description xml:lang="en">Polythematic Structured Subject Heading System (PSH) is as a tool to organize and search for documents by subject. It is a set of subject headings which can be used to describe the document by subject. In its latest version (2.1) PSH is bilingual (Czech-English). Subject headings in both languages are interconnected. PSH contains over 13 000 subject headings and is divided into 44 thematic sections which have been prepared by experts in the respective disciplines in cooperation with librarians. Each subject heading is included in a hierarchy of six (or - under special circumstances - seven) levels according to its semantic content and specificity. The whole system is a tree structure and it represents various concepts from the most general to the more specific ones.</dc:description>
+    <dc:language rdf:resource="http://lexvo.org/id/iso639-3/ces"/>
+    <dc:language rdf:resource="http://lexvo.org/id/iso639-3/eng"/>
+    <dc:language rdf:datatype="http://purl.org/dc/terms/ISO639-2">cze</dc:language>
+    <dc:language rdf:datatype="http://purl.org/dc/terms/ISO639-2">eng</dc:language>
+    <dc:publisher>
+      <rdf:Description>
+        <foaf:mbox rdf:resource="mailto:psh@stk.cz"/>
+        <foaf:name xml:lang="en">National Technical Library</foaf:name>
+        <foaf:name xml:lang="cs">Národní technická knihovna</foaf:name>
+      </rdf:Description>
+    </dc:publisher>
+    <dc:subject rdf:datatype="http://purl.org/dc/terms/LCC">025.43</dc:subject>
+    <dc:subject rdf:datatype="http://purl.org/dc/terms/LCC">Z696.P65</dc:subject>
+    <dc:subject xml:lang="cs">předmětová hesla</dc:subject>
+    <dc:subject xml:lang="en">subject heading system</dc:subject>
+    <dc:subject xml:lang="en">systematic retrieval language</dc:subject>
+    <dc:subject xml:lang="cs">systematický selekční jazyk</dc:subject>
+    <dc:title xml:lang="cs">Polytematický strukturovaný heslář</dc:title>
+    <dc:title xml:lang="en">Polythematic Structured Subject Heading System</dc:title>
+    <dc:type rdf:resource="http://purl.org/dc/dcmitype/Dataset"/>
+    <dcterms:created rdf:datatype="http://www.w3.org/2001/XMLSchema#year">1993</dcterms:created>
+    <dcterms:modified rdf:datatype="http://www.w3.org/2001/XMLSchema#year">2010</dcterms:modified>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH1"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH10067"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH10355"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH1038"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH10652"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH11322"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH11453"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH11591"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH116"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH11939"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH12008"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH12156"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH1217"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH12314"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH12577"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH13220"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH1781"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH2086"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH2395"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH2596"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH2910"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH320"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH3768"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH4231"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH4439"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH5042"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH5176"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH5450"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH573"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH6445"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH6548"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH6641"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH6914"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH7093"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH7769"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH7979"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH8126"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH8308"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH8613"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH8808"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH9194"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH9508"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH9759"/>
+    <skos:hasTopConcept rdf:resource="http://psh.ntkcz.cz/skos/PSH9899"/>
+    <foaf:homepage rdf:resource="http://www.techlib.cz/cs/katalogy-a-databaze/psh/"/>
+  </skos:ConceptScheme>\n\n"""
+
+    skos_dir = os.path.join(settings.ROOT, "static/skos")
+    skos_file = open("%s/psh_skos.rdf" %skos_dir, "w")
+    skos_file.write(header)
+
+    hesla = query_to_dicts("""SELECT id_heslo
+        FROM hesla""")
+    hesla = list(hesla)
+    id_hesel = [heslo["id_heslo"] for heslo in hesla]
+
+    for id_heslo in id_hesel:
+        print id_heslo
+        heslo = get_concept_as_dict(id_heslo)
+
+        skos_file.write("".join(['<skos:Concept rdf:about="http://psh.ntkcz.cz/skos/', heslo["id_heslo"],'">\n']))
+        skos_file.write('<skos:inScheme rdf:resource="http://psh.ntkcz.cz/skos/"/>\n')
+        skos_file.write("".join(['<dc:identifier>', heslo["id_heslo"],'</dc:identifier>\n']))
+        skos_file.write("".join(['<skos:prefLabel xml:lang="cs">', heslo["heslo"],'</skos:prefLabel>\n']).encode("utf8"))
+        skos_file.write("".join(['<skos:prefLabel xml:lang="en">', heslo["ekvivalent"],'</skos:prefLabel>\n']).encode("utf8"))
+        for varianta in heslo["varianty"]:
+            skos_file.write("".join(['<skos:altLabel xml:lang="', varianta["jazyk"],'">', varianta["varianta"],'</skos:altLabel>\n']).encode("utf8"))
+
+        for podrazeny in heslo["podrazeny"]:
+            skos_file.write("".join(['<skos:narrower rdf:resource="http://psh.ntkcz.cz/skos/', podrazeny,'"/>\n']))
+
+        for pribuzny in heslo["pribuzny"]:
+            skos_file.write("".join(['<skos:related rdf:resource="http://psh.ntkcz.cz/skos/', pribuzny,'"/>\n']))
+
+        if heslo["nadrazeny"]:
+            skos_file.write("".join(['<skos:broader rdf:resource="http://psh.ntkcz.cz/skos/', heslo["nadrazeny"],'"/>\n']))
+        skos_file.write("</skos:Concept>\n\n")
+
+    skos_file.write("</rdf:RDF>")
+    skos_file.close()
+
+    skos_dir = os.path.join(settings.ROOT, "static/skos")
+    os.system("zip -j %s/psh_skos.zip %s/psh_skos.rdf" %(skos_dir, skos_dir))
 
 if __name__ == "__main__":
     update()
+    make_skos()
